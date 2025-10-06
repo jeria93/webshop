1a7e4d81d4ef5bbe9e8afb4e060df3e1364386d9
Zeldisen <jeanette.norberg82@gmail.com>
2025-10-06 11:40:36 +0200

    added jsx andcss for wishlist

---
+++ src/components/WishList.jsx
@@ -0,0 +1,12 @@
+function WishList(){
+   
+    return(
+        <>
+        <section className="wishlist">
+        <h1>Wish List</h1>
+        </section>
+
+        </>
+    )
+}
+export default WishList;
