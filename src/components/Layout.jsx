9c322d3919f58cd580d8319bd4bedeac95f105ba
angie <angelica.ekman@hotmail.com>
2025-10-07 16:26:06 +0200

    created account & cart jsx + css. Navigation on navbar to them

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,18 @@
 import { Link, Outlet } from "react-router-dom";
 import "./Layout.css";
 
+
+
 export default function Layout() {
   return (
     <div className="layout-container">
       <nav className="navbar"> 
         <Link to="/">Hem</Link>
         <Link to="/wishList">Önskelista</Link>
-        <Link to="/movieDetails">Kundvagn</Link>
+        <Link to="/cart">Kundvagn</Link>
+        <Link to="/account">Konto</Link>
+        
         <Link to="/movieDetails">Sök</Link>
-        <Link to="/movieDetails">Konto</Link>
       </nav>
 
       {/* Margin för content under navbar */}
---
+++ src/pages/cart.jsx
@@ -0,0 +1,11 @@
+import './cart.css';
+
+const Cart = () => {
+    return (
+        <div>
+            <h1>Cart</h1>
+        </div>
+    );
+};
+
+export default Cart;
