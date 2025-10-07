c364e219d4747df2fda0182761c751fd1523370e
Zeldisen <jeanette.norberg82@gmail.com>
2025-10-07 13:11:57 +0200

    added some css

--- src/pages/WishList.jsx
+++ src/pages/WishList.jsx
@@ -1,11 +1,35 @@
 import { Link } from "react-router-dom";
 import "./wishList.css";
+import { Heart, Trash2} from "lucide-react";
+import { useState } from "react";
 
-function WishList() {
+function WishList({defaultLiked = false, onChange, onDelete}) {
+  const [liked, setLiked] = useState(defaultLiked);
+
+  const toggle = () => {
+    const next =!liked;
+    setLiked(next);
+      onChange?.(next);
+  };
+
   return (
     <>
       <section className="wishlist">
-        <h1>Wish List</h1>
+
+        <h1 className="rubrik">Önksade artiklar</h1>
+        <section className="card">
+          <section className="buttons">
+          <button onClick={toggle}
+           aria-pressed={liked} 
+          aria-label={liked? "tabort gilla" : "gilla"}
+          className="icon-btn icon-btn--heart">
+            <Heart className="heart-btn" aria-hidden="true"/>
+          </button>
+          <button className="icon-btn"onClick={onDelete}> <Trash2 className="trash-btn"/> </button>
+          </section>
+
+        </section>
+
       </section>
       <Link to="/">Home</Link>
     </>
