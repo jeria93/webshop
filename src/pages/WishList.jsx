47f8d1231e12802a89c132520dfad16fd8867047
Daffnel <3ggapp24.andrda@folkuniversitetet.nu>
2025-10-07 15:56:03 +0200

    Fixed conflict WishList.jsx

--- src/pages/WishList.jsx
+++ src/pages/WishList.jsx
@@ -41,11 +41,7 @@ function WishList({defaultLiked = false, onChange, onDelete}) {
         </section>
         <Link className={s.linkBtn} to="/"><ArrowLeftIcon/> Tillbaka</Link>
       </section>
-<<<<<<< HEAD
-      
-=======
 
->>>>>>> origin/dev-main
     </>
   );
 }
