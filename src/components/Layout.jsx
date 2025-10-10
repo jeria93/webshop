8a51c776372f8e7b7da0a9e378928cbb89ee53e1
Zeldisen <jeanette.norberg82@gmail.com>
2025-10-10 17:13:19 +0200

    mobile adapt navbar and diffrent colors and logo

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,11 +2,14 @@ import { Link, Outlet } from "react-router-dom";
 import { useState } from "react";
 import "./layout.css";
 import ShoppingCart from "./ShoppingCart/ShoppingCart";
-import {UserRound, Search, ShoppingBasket, Star, House} from "lucide-react";
-import logo from "../assets/MovieCart.png"
+import {UserRound, Search, ShoppingBasket, Star, House, Menu, X} from "lucide-react";
+import logo from "../assets/MovieCart5.png"
 
 export default function Layout() {
   const [showShoppingCart, SetShowShoppingCart] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const closeMenu = () => setMenuOpen(false);
 
   return (
     <div className="layout-container">
@@ -19,19 +22,26 @@ export default function Layout() {
 
       <nav className="navbar"> 
         <img className="logoImg" src={logo} alt="MovieCart"></img>
-        
-        <div className="navCenter">
-        <Link className="linkIcon" to="/"><House/>Hem</Link>
-        <Link className="linkIcon" to="/wishList"><Star/>Favoriter</Link>
+
+        {/* Mobilanpassad knapp för att få upp menyn/ förvinner om skärmen är större */}
+        <button className="menuButton" aria-expanded={menuOpen} aria-controls="navCenter"
+        onClick={() => setMenuOpen((v) => !v)}>
+          {menuOpen ? <X aria-hidden /> : <Menu aria-hidden/>}
+        </button>
+
+        <div id="navCenter" className={`navCenter ${menuOpen ? "is-open" : ""}`}>
+        <Link className="linkIcon" to="/" onClick={closeMenu}><House/>Hem</Link>
+        <Link className="linkIcon" to="/wishList" onClick={closeMenu}><Star/>Favoriter</Link>
         
         {/*Logik för varukorgen */}
         <Link  className="linkIcon" onClick={() => {
           SetShowShoppingCart(true);
+          closeMenu();
         }} ><ShoppingBasket/>Varukorg</Link>
 
 
-        <Link className="linkIcon" to="/account"><UserRound/>Konto</Link>
-        <Link className="linkIcon" to="/search"><Search/>Sök</Link>
+        <Link className="linkIcon" to="/account" onClick={closeMenu}><UserRound/>Konto</Link>
+        <Link className="linkIcon" to="/search" onClick={closeMenu}><Search/>Sök</Link>
         </div>
       </nav>
 
